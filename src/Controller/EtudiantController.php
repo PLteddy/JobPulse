7549bb27664e5d55d23d31fc0c376796b3e0037a
Melina38 <melinabascol@gmail.com>
2025-04-09 16:53:55 +0200

    ADD profil candidatures enregistrements tuteur .html.twig
    et Update index etudiant + header etu + header

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\PosteRepository; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,15 +9,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class EtudiantController extends AbstractController
 {
     #[Route('/etudiant', name: 'etudiant_dashboard')]
-    public function index(PosteRepository $posteRepository): Response
+    public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ETUDIANT');
-        
-        // Récupérer toutes les offres (postes) de toutes les entreprises
-        $postes = $posteRepository->findAll();
-        
-        return $this->render('etudiant/index.html.twig', [
-            'postes' => $postes,
-        ]);
+
+        // Logique pour afficher le tableau de bord étudiant
+        return $this->render('etudiant/index.html.twig');
+    }
+
+    #[Route('/etudiant/candidatures', name: 'etudiant_candidatures')]
+    public function candidatures(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ETUDIANT');
+
+        // Logique pour afficher les candidatures de l'étudiant
+        return $this->render('etudiant/candidatures.html.twig');
+    }
+
+    #[Route('/etudiant/enregistrements', name: 'etudiant_enregistrements')]
+    public function enregistrements(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ETUDIANT');
+
+        // Logique pour afficher les enregistrements de l'étudiant
+        return $this->render('etudiant/enregistrements.html.twig');
+    }
+
+    #[Route('/etudiant/tuteur', name: 'etudiant_tuteur')]
+    public function tuteur(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ETUDIANT');
+
+        // Logique pour afficher les informations sur le tuteur
+        return $this->render('etudiant/tuteur.html.twig');
+    }
+
+    #[Route('/etudiant/profil', name: 'etudiant_profil')]
+    public function profil(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ETUDIANT');
+
+        // Logique pour afficher les informations sur le tuteur
+        return $this->render('etudiant/profil.html.twig');
     }
 }
