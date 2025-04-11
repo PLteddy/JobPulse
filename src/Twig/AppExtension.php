66e83100356507121098b06662e5d62b1591f5b8
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-04-11 09:42:37 +0200

    ADD UPDATE
    footer de chaque type d'utilisateur.
    correction d'offre détails.

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -19,6 +19,7 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFunction('getHeaderTemplate', [$this, 'getHeaderTemplate']),
+            new TwigFunction('getFooterTemplate', [$this, 'getFooterTemplate']),
         ];
     }
 
@@ -45,5 +46,29 @@ class AppExtension extends AbstractExtension
         
         // Header par défaut
         return 'partials/header.html.twig';
+    }
+    public function getFooterTemplate(): string
+    {
+        $user = $this->security->getUser();
+        
+        if (!$user) {
+            return 'partials/footer.html.twig';
+        }
+        
+        // Adapte cette logique en fonction de ton entité utilisateur
+        if (method_exists($user, 'getRoles')) {
+            $roles = $user->getRoles();
+            
+            if (in_array('ROLE_ETUDIANT', $roles)) {
+                return 'partials/footer_etu.html.twig';
+            } elseif (in_array('ROLE_TUTEUR', $roles)) {
+                return 'partials/footer_tuteur.html.twig';
+            } elseif (in_array('ROLE_ENTREPRISE', $roles)) {
+                return 'partials/footer_entreprise.html.twig';
+            }
+        }
+        
+        // Header par défaut
+        return 'partials/footer.html.twig';
     }
 }
